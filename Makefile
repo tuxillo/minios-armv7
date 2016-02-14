89e3282dcb484e663452d0170a03618aaf8bcd13
Antonio Huete Jimenez <tuxillo@quantumachine.net>
2016-02-14 22:46:58 +0000

    Start using a ld script

--- Makefile
+++ Makefile
@@ -13,7 +13,9 @@ CFLAGS+= -mcpu=cortex-a15
 boot.elf: subr_prf.o boot.o
 	$(TOOLCHAIN)gcc -c subr_prf.c $(CFLAGS)
 	$(TOOLCHAIN)gcc -c boot.c $(CFLAGS) boot.c
-	$(TOOLCHAIN)gcc subr_prf.o boot.c $(CFLAGS) -o boot.elf
+	$(TOOLCHAIN)as startup.S -o startup.o
+	$(TOOLCHAIN)ld -T boot.ld -nodefaultlibs -nostdlib \
+			startup.o boot.o subr_prf.o -o boot.elf
 
 clean:
 	-rm -f boot.elf *.o
--- boot.c
+++ boot.c
@@ -1,38 +1,13 @@
 #include "boot.h"
 #include "subr.h"
 
-extern void *__bss_start;
-extern void *__bss_end__;
-uint8_t __stack[512] __attribute__((aligned(4), section(".stack")));
-
 void kern_boot(void);
-void _start(void) __attribute__ ( ( naked ) );
 
-const char *msg = "Hello world!\n";
+char *msg = "Hello world!\n";
 int myvar = 16;
-int foo;
-
-/*
-int
-task(void) {
-
-	return 0;
-}
-*/
 
 void
-clearbss(void)
-{
-	unsigned int *start = (unsigned int *)&__bss_start;
-	unsigned int *end = (unsigned int *)&__bss_end__;
-	unsigned int *p;
-
-	for (p = start; p < end; p++)
-		*p = 0x00;
-}
-
-void
-testargs(const char *s, int *a)
+testargs(char *s, int *a)
 {
 	const char *p;
 	int *p1;
@@ -46,21 +21,11 @@ testargs(const char *s, int *a)
 	p1 = a;
 }
 
-void
-_start(void)
-{
-
-	asm volatile("ldr sp, =0x80200");
-
-	clearbss();
-
-	kern_boot();
-}
 
 void
 kern_boot(void)
 {
-	testargs("hello", &myvar);
+	testargs(msg, &myvar);
 
 	/* Loop forever */
 	for(;;);
