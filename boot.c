8762979048ec94a2d7e76b356c45379f2659c660
Antonio Huete Jimenez <tuxillo@quantumachine.net>
2016-02-16 15:33:32 +0000

    Fix entry point in the ld script

--- boot.c
+++ boot.c
@@ -3,29 +3,11 @@
 
 void kern_boot(void);
 
-char *msg = "Hello world!\n";
-int myvar = 16;
-
-void
-testargs(char *s, int *a)
-{
-	const char *p;
-	int *p1;
-
-	p = s;
-	if (s == NULL)
-		*uart_base = (int)'0';
-	else
-		*uart_base = (int)'1';
-
-	p1 = a;
-}
-
-
 void
 kern_boot(void)
 {
-	testargs(msg, &myvar);
+
+	kprintf("miniOS arvm7 startup ...\n");
 
 	/* Loop forever */
 	for(;;);
